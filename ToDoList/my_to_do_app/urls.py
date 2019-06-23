da4d563a9aca71cdd66f3b28cacf99931a49b16a
doorBW <doorbw@outlook.com>
2019-06-23 16:54:15 +0900

    todo save to db

--- ToDoList/my_to_do_app/urls.py
+++ ToDoList/my_to_do_app/urls.py
@@ -1,6 +1,8 @@
+# my_to_do_app > urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('createTodo/', views.createTodo)
 ]
--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,6 +1,14 @@
+# my_to_do_app > views.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'my_to_do_app/index.html')
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponse("create Todo를 할거야! =>"+user_input_str)
